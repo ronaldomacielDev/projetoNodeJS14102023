d4b42bc24844c69c741e73803fadf67784cafe55
Ronaldo Maciel <140128563+ronaldomacielDev@users.noreply.github.com>
2023-10-15 11:20:28 -0300

    created Criptografia da senha
    created
    Criptografia da senha +
    e-mail único no banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import { pool } from './mysql';
-import { v4 as uuidv4 } from 'uuid';
+import { pool } from './mysql'; //
+import { v4 as uuidv4 } from 'uuid'; // biblioteca para criar id unicos
+import { hash } from 'bcrypt'; // biblioteca para criptografia da senha
 
 const app = express();
 
@@ -9,18 +10,23 @@ app.use(express.json());
 app.post('/user', (request, response) => {
     const { name, email, password } = request.body;
     pool.getConnection((err: any, connection: any) => {
-        connection.query(
-            'INSERT INTO users ( user_id, name, email, password ) VALUES (?,?,?,?)',
-            [uuidv4(), name, email, password ],
-            (error: any, result: any, fileds: any ) => {
-                if (error) {
-                    return response.status(400).json(error)
-                }
-                response.status(200).json({ success: true});
+        hash(password, 10, (err, hash) => {
+            if(err) {
+                return response.status(500).json(err)
             }
-        )
-    })
 
+            connection.query(
+                'INSERT INTO users ( user_id, name, email, password ) VALUES (?,?,?,?)',
+                [uuidv4(), name, email, hash ],
+                (error: any, result: any, fileds: any ) => {
+                    if (error) {
+                        return response.status(400).json(error)
+                    }
+                    response.status(200).json({ success: true});
+                }
+            )
+        })
+    })
 })
 
 app.listen(4000);
